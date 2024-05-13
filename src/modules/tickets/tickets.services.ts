efb955a93407714d14c18ab886e8430f63f5b651
hernanconiglio <hernanconiglio@gmail.com>
2024-05-12 23:25:52 -0300

    modules

---
+++ src/modules/tickets/tickets.services.ts
@@ -0,0 +1,47 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Ticket, TicketDocument } from './tickets.schema';
+import { CreateTicketDto } from './dto/create-ticket.dto';
+import { UpdateTicketDto } from './dto/update-ticket.dto';
+
+@Injectable()
+export class TicketService {
+  constructor(
+    @InjectModel(Ticket.name) private readonly ticketModel: Model<TicketDocument>,
+  ) { }
+
+  async findAll(): Promise<Ticket[]> {
+    return this.ticketModel.find().exec();
+  }
+
+  async findById(id: string): Promise<Ticket> {
+    const ticket = await this.ticketModel.findById(id).exec();
+    if (!ticket) {
+      throw new NotFoundException('Ticket no encontrado');
+    }
+    return ticket;
+  }
+
+  async create(createTicketDto: CreateTicketDto): Promise<Ticket> {
+    const createdTicket = new this.ticketModel(createTicketDto);
+    return createdTicket.save();
+  }
+
+  async update(id: string, updateTicketDto: UpdateTicketDto): Promise<Ticket> {
+    const updatedTicket = await this.ticketModel.findByIdAndUpdate(id, updateTicketDto, { new: true }).exec();
+    if (!updatedTicket) {
+      throw new NotFoundException('Ticket no encontrado');
+    }
+    return updatedTicket;
+  }
+
+  async delete(id: string): Promise<Ticket> {
+    const deletedTicket = await this.ticketModel.findByIdAndDelete(id).exec();
+    if (!deletedTicket) {
+      throw new NotFoundException('Ticket no encontrado');
+    }
+    return deletedTicket;
+  }
+}
