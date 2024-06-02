eed4ddfa6d95d21664b8881b10902f626675638b
hernanconiglio <hernanconiglio@gmail.com>
2024-06-02 15:39:07 -0300

    cambios en controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@
 import { Module } from '@nestjs/common';
 import { EventModule } from './modules/events/events.module';
 import { TicketModule } from './modules/tickets/tickets.module';
+import { AppController } from './controllers/app.controller';
 
 @Module({
   imports: [
     EventModule,
-    TicketModule,
+    TicketModule
   ],
+  controllers: [AppController]
 })
 export class AppModule { }
