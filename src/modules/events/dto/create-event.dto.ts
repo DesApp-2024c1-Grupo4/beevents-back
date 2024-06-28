c073ea7c4358211016ef6bb158fa37d7d2ac1582
Coniglio Hernan Marcelo <CONIGLIOH@tasa.telefonica.com.ar>
2024-06-28 09:16:34 -0300

    tickets en eventos

--- src/modules/events/dto/create-event.dto.ts
+++ src/modules/events/dto/create-event.dto.ts
@@ -4,13 +4,22 @@ export class CreateEventDto {
     readonly name: string;
     readonly artist: string;
     readonly image: string;
+    readonly description: string;
     readonly location_id: string;
     readonly user_id: string;
-    readonly date_times: Date[];
-    readonly sectors: {
-        name: string;
-        numbered: boolean;
-        rows: number;
-        seats: number;
+    readonly dates: {
+        date_time: Date;
+        sectors: {
+            name: string;
+            rowsNumber: number;
+            seatsNumber: number;
+            available: number;
+            rows: {
+                displayId: string;
+                available: boolean;
+                timestamp: Date;
+                reservedBy: string;
+            }[];
+        }[];
     }[];
 }
--- src/modules/events/events.schema.ts
+++ src/modules/events/events.schema.ts
@@ -6,6 +6,50 @@ import { Document } from 'mongoose';
 export type EventDocument = Event & Document;
 
 @Schema()
+export class Sector extends Document {
+    @Prop({ type: String, required: true })
+    name: string;
+
+    @Prop({ type: Boolean, required: true })
+    numbered: boolean;
+
+    @Prop({ type: Number, required: true })
+    rowsNumber: number;
+
+    @Prop({ type: Number, required: true })
+    seatsNumber: number;
+
+    @Prop({ type: Number, required: false })
+    available: number;
+
+    @Prop([{
+        displayId: { type: String, required: false },
+        available: { type: Boolean, required: true },
+        timestamp: { type: Date, required: true },
+        reservedBy: { type: String, required: true }
+    }])
+    rows: {
+        displayId: string;
+        available: boolean;
+        timestamp: Date;
+        reservedBy: string;
+    }[];
+}
+
+const SectorSchema = SchemaFactory.createForClass(Sector);
+
+@Schema()
+export class Dates {
+    @Prop({ type: String, required: true })
+    date_time: Date;
+
+    @Prop({ type: [SectorSchema], required: true })
+    sectors: Sector[];
+}
+
+const DatesSchema = SchemaFactory.createForClass(Dates);
+
+@Schema()
 export class Event {
     @Prop({ required: true })
     name: string;
@@ -17,28 +61,56 @@ export class Event {
     image: string;
 
     @Prop({ required: true })
+    description: string;    
+
+    @Prop({ required: true })
     location_id: string;
 
     @Prop({ required: true })
     user_id: string;
 
-    @Prop([Date])
-    date_times: Date[];
-
-    @Prop([{
-        name: { type: String, required: true },
-        numbered: { type: Boolean, required: true},
-        rows: { type: Number, required: true},
-        seats: { type: Number, required: true}
-    }])
-    sectors: {
-        name: string;
-        numbered: boolean;
-        rows: number;
-        seats: number
-    }[];
-
+    @Prop({ type: [DatesSchema], required: true })
+    dates: Dates[];
+    
     // Otras propiedades y métodos según sea necesario
 }
 
 export const EventSchema = SchemaFactory.createForClass(Event);
+
+// Función para convertir un número a una secuencia alfabética
+function numberToAlphabet(num: number): string {
+    let str = '';
+    while (num >= 0) {
+        str = String.fromCharCode((num % 26) + 65) + str;
+        num = Math.floor(num / 26) - 1;
+    }
+    return str;
+}
+
+// Middleware pre-save para establecer `available` y crear los asientos si `numbered` es true
+EventSchema.pre<EventDocument>('save', function(next) {
+    if (this.isNew) {
+        this.dates.forEach(dateItem => {
+            dateItem.sectors.forEach(sector => {
+                // Establecer la propiedad `available` siempre
+                sector.available = sector.rowsNumber * sector.seatsNumber;
+
+                // Si `numbered` es true, crear los asientos
+                if (sector.numbered) {
+                    for (let i = 0; i < sector.rowsNumber; i++) {
+                        const rowLabel = numberToAlphabet(i);
+                        for (let j = 0; j < sector.seatsNumber; j++) {
+                            sector.rows.push({
+                                displayId: `${rowLabel}-${j + 1}`,
+                                available: true,
+                                timestamp: new Date(),
+                                reservedBy: ''
+                            });
+                        }
+                    }
+                }
+            });
+        });
+    }
+    next();
+});
