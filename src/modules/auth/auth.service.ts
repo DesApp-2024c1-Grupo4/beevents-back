0ce2c8b210afe90375fd9d55a259d086f3edb6b7
mauriciopintos <mauricioenrique.pintos@estudiantes.unahur.edu.ar>
2024-08-26 18:11:53 -0300

    Validacion de email existente

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../users/users.services';
 import { CreateUserDto } from '../users/dto/create-user.dto';
@@ -10,7 +10,7 @@ export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly userService: UserService,
-  ) {}
+  ) { }
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.userService.findByEmail(email);
@@ -24,19 +24,33 @@ export class AuthService {
   async login(user: any) {
     const payload = { username: user.email, sub: user._id, role: user.role }; // Incluye 'role' en el payload
     return {
-        access_token: this.jwtService.sign(payload),
-        role: user.role
+      access_token: this.jwtService.sign(payload),
+      role: user.role
     };
-}
+  }
 
   async register(userDto: CreateUserDto): Promise<User> {
+    // Verificar si el email ya existe
+    const existingUser = await this.userService.findByEmail(userDto.email);
+    if (existingUser) {
+      throw new ConflictException('El email ingresado ya existe. Ingrese otro email o intente recuperar su contraseña');
+    }
+    // Si el email no existe, se procede a crear el usuario
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(userDto.password, salt);
     const newUser = await this.userService.create({
-        ...userDto,
-        password: hashedPassword
+      ...userDto,
+      password: hashedPassword,
     });
     return newUser;
-}
-  
+  }
+
+  // const salt = await bcrypt.genSalt();
+  // const hashedPassword = await bcrypt.hash(userDto.password, salt);
+  //   const newUser = await this.userService.create({
+  //       ...userDto,
+  //       password: hashedPassword
+  //   });
+  //   return newUser;
+
 }
